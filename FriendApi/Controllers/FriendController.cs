b83ac6dbe7886aeb9cbd26f8f47357349c8c4454
Thomas Baminger <thms.baminger@gmail.com>
2021-03-11 12:14:01 +0100

    Add authorization to FriendController

---
+++ FriendApi/Controllers/FriendController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using FriendApi.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendApi.Controllers
+{
+    [ApiController]
+    [Route("api/friend")]
+    [Authorize]
+    public class FriendController : ControllerBase
+    {
+        private readonly FriendDbContext _context;
+
+        public FriendController(FriendDbContext context)
+        {
+            _context = context;
+        }
+        
+        [Authorize]
+        public IEnumerable<Friend> GetAllFriends()
+        {
+            return _context.Friends;
+        }
+    }
+}
